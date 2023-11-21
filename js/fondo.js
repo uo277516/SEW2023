8a62998079c87406a5b4cd35fcc1c7aebfc8611f
Natalia Fernández Riego <90771958+uo277516@users.noreply.github.com>
2023-11-21 02:30:38 +0100

    Add files via upload

--- js/fondo.js
+++ js/fondo.js
@@ -1,8 +1,41 @@
-"use strict";
 class Fondo {
     constructor(nombre_pais, nombre_capital, coordenadas) {
         this.nombre_pais = nombre_pais;
-        this.nombre_capital=nombre_capital;
-        this.coordenadas=coordenadas;
+        this.nombre_capital = nombre_capital;
+        this.coordenadas = coordenadas;
+    }
+
+    obtenerImagen() {
+        const coordenadasArray = this.coordenadas.split(',').map(coord => parseFloat(coord.trim()));
+
+        const latitud = coordenadasArray[0];
+        const longitud = coordenadasArray[1];
+
+        var apiKey = "7ea9c232e677033d7c7fe0fbc5f8887b";
+        var flickUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search";
+        
+        $.getJSON(flickUrl, {
+            api_key: apiKey,
+            lat: latitud,
+            lon: longitud,
+            tags: "landscape",  
+            format: "json",
+            nojsoncallback: 1  
+        })
+        .done(function (data) {
+            if (data.photos && data.photos.photo.length > 0) {
+                const photo = data.photos.photo[5];
+                const imageUrl = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
+                console.log(photo);
+                $("body").css("background-image", "url(" + imageUrl+ ")") 
+                    .css("background-size","cover")
+                    .css("background-repeat","no-repeat")
+                    .css("background-attachment","fixed")
+            }
+        });
     }
 }
+
+
+var fondo = new Fondo("Camboya", "Phnom Penh", "11.56245, 104.91601, 10");
+fondo.obtenerImagen();
--- js/pais.js
+++ js/pais.js
@@ -5,7 +5,7 @@ class Pais {
             this.nombre=nombre;
             this.capital=capital;
             this.poblacion=poblacion;
-        }
+    }
 
     rellenar (forma_gobierno, coordenadas_capital, religion) {
         this.forma_gobierno=forma_gobierno;
@@ -56,12 +56,87 @@ class Pais {
         document.write(this.getCapital());
         document.write("</p>");
 
-        document.write("<p>Población: ");
-        document.write(this.getPoblacion());
-        document.write(" personas </p>");
-
         document.write(this.getListaInformacionSecundaria());
     }
+
+    cargarTiempo() {
+        const coordenadasArray = this.coordenadas_capital.split(',').map(coord => parseFloat(coord.trim()));
+
+        const latitud = coordenadasArray[0];
+        const longitud = coordenadasArray[1];
+
+        var apiKey = "6049eddf2521839fe5a2fdf60aaf706f";
+        var apiCall = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitud}&lon=${longitud}&appid=${apiKey}`;
+        console.log(apiCall);
+        
+        $.ajax({
+            url: apiCall,
+            method: 'GET',
+            dataType: "json",
+            data: {
+                units: "metric",
+                lang: "es"
+            },
+            success: function(datos){
+
+                console.log(datos.list);
+                var main = $("<main></main>");
+                var h3 = $("<h3></h3>").text("Tabla del tiempo para los próximos 5 días");
+                $("body").append(h3,main);
+
+                var pronostico = datos.list;
+
+                for (let i = 0; i<40; i=i+8) {   
+                    var dia = pronostico.slice(i,i+8); 
+                    
+                    for (let j = 0; j<dia.length; j++) { 
+
+                        var dia_hora_mostrar = dia[j].dt_txt;
+                        var dia_hora_mostrar_string = String(dia_hora_mostrar)
+                        
+
+                        if (dia_hora_mostrar_string.includes("06:00:00")) {
+
+                            var cantidadLluvia = 0;
+                            if (dia[j].rain  !== undefined) {
+                                cantidadLluvia = dia[j].rain['3h'];
+                            }
+
+                            var fecha = dia_hora_mostrar_string.split(" ")[0].split("-");
+
+                            const articleElement = $("<article>");
+                            var h4 = $("<h4></h4>").text(fecha[2]+"-"+fecha[1]+"-"+fecha[0]);
+                            var ul = $("<ul><ul>")
+                            var tempMax = $("<li></li>").text("Temperatura máxima: " + dia[j].main.temp_max + "°C");
+                            var tempMin = $("<li></li>").text("Temperatura mínima: " + dia[j].main.temp_min + "°C");
+                            var humedad = $("<li></li>").text("Humedad: " + dia[j].main.humidity + "%");
+                            var iconoTiempo = $('<img/>').attr("src", "https://openweathermap.org/img/wn/"+ dia[j].weather[0].icon+ "@2x.png").attr("alt",dia[j].weather[0].description);
+                            var lluvia = $("<li></li>").text("Cantidad de lluvia: " + cantidadLluvia + "mm");
+
+                            ul.append(tempMax, tempMin, humedad, lluvia);
+                            articleElement.append(h4,iconoTiempo, ul);
+                            $("main").append(articleElement);
+
+                    }
+                }
+            }
+
+
+
+            // const pElement = $("<p>").text(JSON.stringify(datos.list, null, 2));
+
+            //sectionElement.append(pElement);
+            //$("body").append(sectionElement);
+
+            },
+            error:function(){
+                $("h3").html("¡Tenemos problemas! No puedo obtener JSON de <a href='http://openweathermap.org'>OpenWeatherMap</a>");
+            }
+        });
+        
+    }
+
+
 
 }
 
